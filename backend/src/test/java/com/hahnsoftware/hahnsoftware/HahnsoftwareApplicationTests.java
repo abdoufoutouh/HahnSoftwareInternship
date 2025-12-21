9aa380995554dda94179eb304a9d64113714a791
Abderrahmane <foutouhabderrahman8@gmail.com>
2025-12-21 11:50:38 +0100

    chore: initial setup

---
+++ backend/src/test/java/com/hahnsoftware/hahnsoftware/HahnsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahnsoftware.hahnsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HahnsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
